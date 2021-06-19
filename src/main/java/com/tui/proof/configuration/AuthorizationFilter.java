b21f445bc3648ff8bfed9636947e131dc9a06bce
Aliaksei Khanko <alex.khanko@gmail.com>
2021-06-19 18:24:46 +0300

    fix authorization
    TUI-1 Create a new API for query availability and booking of flights

--- src/main/java/com/tui/proof/configuration/AuthorizationFilter.java
+++ src/main/java/com/tui/proof/configuration/AuthorizationFilter.java
@@ -13,6 +13,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -23,14 +24,19 @@ public class AuthorizationFilter implements Filter {
     @Value("${api.authorization.header}")
     private String authorizationHeader;
 
+    @Value("#{'${api.authorization.urls}'.split(',')}")
+    private List<String> authorizationUrls;
+
     @Override
     public void doFilter(ServletRequest servletRequest,
                          ServletResponse servletResponse,
                          FilterChain filterChain)
             throws ServletException, IOException {
         String authorizationToken = ((HttpServletRequest) servletRequest).getHeader(authorizationHeader);
+        String requestURI = ((HttpServletRequest) servletRequest).getRequestURI();
+        boolean needAuthorization = authorizationUrls.stream().anyMatch(requestURI::contains);
 
-        if (!securityService.authenticate(authorizationToken)) {
+        if (needAuthorization && !securityService.authenticate(authorizationToken)) {
             HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
             httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED);
             return;
