69adca3361630aaac6bf68bef9d257c7bc5e29dc
Aliaksei Khanko <alex.khanko@gmail.com>
2021-06-19 17:20:26 +0300

    refactor exceptions
    TUI-1 Create a new API for query availability and booking of flights

--- src/main/java/com/tui/proof/exception/ExceptionDto.java
+++ src/main/java/com/tui/proof/exception/ExceptionDto.java
@@ -3,6 +3,9 @@ package com.tui.proof.exception;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,4 +18,22 @@ public class ExceptionDto {
     private final LocalDateTime timestamp;
     private final int status;
     private final List<String> messages;
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof ExceptionDto) {
+            return EqualsBuilder.reflectionEquals(this, obj);
+        }
+        return false;
+    }
 }
